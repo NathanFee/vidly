5bb082775acd30da0f56f8400489737c0d790e67
NathanFee <Nathan_Fee@yahoo.com>
2019-06-20 15:22:30 -0400

    Initial Commit

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,9 +1,13 @@
 import React, { Component, Fragment } from "react";
 import { getMovies } from "../services/fakeMovieService.js";
+import Liked from "./common/like";
+import Pagination from "./common/pagination.jsx";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    pageSize: 4,
+    currentPage: 1
   };
 
   handleDelete = movie => {
@@ -11,11 +15,25 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
+  handleLike = movie => {
+    console.log(movie);
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
     const { length: count } = this.state.movies;
-    return count === 0 ? (
-      "There are no movies in the database."
-    ) : (
+    const { pageSize, currentPage } = this.state;
+    if (count === 0) return "There are no movies in the database.";
+    const movies = "";
+    return (
       <Fragment>
         <p>Showing {count} movies in the database.</p>
         <table className="table">
@@ -26,6 +44,7 @@ class Movies extends Component {
               <th>Genre</th>
               <th>Stock</th>
               <th>Rate</th>
+              <th />
               <th />
             </tr>
           </thead>
@@ -39,6 +58,12 @@ class Movies extends Component {
                   <td>{movie.numberInStock}</td>
                   <td>{movie.dailyRentalRate}</td>
                   <td>
+                    <Liked
+                      liked={movie.liked}
+                      onClick={() => this.handleLike(movie)}
+                    />
+                  </td>
+                  <td>
                     <button
                       onClick={() => this.handleDelete(movie)}
                       className="btn btn-danger"
@@ -51,6 +76,12 @@ class Movies extends Component {
             })}
           </tbody>
         </table>
+        <Pagination
+          onPageChange={this.handlePageChange}
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+        />
       </Fragment>
     );
   }
