cb89434cd08577c0ef7e34104bb38f0c8f7d73d7
NathanFee <Nathan_Fee@yahoo.com>
2019-06-24 17:19:41 -0400

    Adds routing to App and basic components
    - Adds simple Customers, Rentals, NotFound and MovieForm components.
    - Creates NavLinks for each component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
 import "./App.css";
-import Movies from "./components/Movies";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect exact from="/" to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </Fragment>
     );
   }
 }
