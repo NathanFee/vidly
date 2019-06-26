2db6c74073c17af2e5088d96366ad678e47ae925
NathanFee <Nathan_Fee@yahoo.com>
2019-06-26 09:03:26 -0400

    Adds a login and input component for login feature

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import Customers from "./customers";
 
 const NavBar = () => {
   return (
@@ -34,6 +33,11 @@ const NavBar = () => {
           <li className="nav-item">
             <NavLink className="nav-link" to="/rentals">
               Rentals
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/login">
+              Login
             </NavLink>
           </li>
         </ul>
