034750a06f7e53b650cdf23a3a6684c3c240f519
NathanFee <Nathan_Fee@yahoo.com>
2019-07-09 11:20:41 -0400

    Adds logout feature
    - Adds logout component and route

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegistrationForm from "./components/registrationForm";
+import Logout from "./components/logout";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
@@ -36,6 +37,7 @@ class App extends Component {
           <Switch>
             <Route path="/register" component={RegistrationForm} />
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies/new" component={MovieForm} />
             <Route path="/movies" component={Movies} />
