659742a9b1442df645fdd4435251ecb08c481343
NathanFee <Nathan_Fee@yahoo.com>
2019-07-09 09:12:12 -0400

    Creates save movie feature
    - Saves movie to server on update or adding a new movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,20 +3,28 @@ import { apiUrl } from "../config.json";
 
 const apiEndPoint = apiUrl + "/movies";
 
+function movieUrl(id) {
+  return `${apiEndPoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(apiEndPoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndPoint + "/" + movieId);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
-    http.put(apiEndPoint + "/" + movie._id, movie);
+    const body = { ...movie };
+    delete body._id;
+    return http.put(movieUrl(movie._id), body);
   }
+
+  return http.post(apiEndPoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiUrl + "/" + movieId);
+  return http.delete(movieUrl(movieId));
 }
